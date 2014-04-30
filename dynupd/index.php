6e4e697a18ac4add2325db6da73878ac462bac3b
mjh1709muc <m@henne.eu>
2014-04-30 23:13:24 +0200

    Update index.php

--- dynupd/index.php
+++ dynupd/index.php
@@ -32,17 +32,6 @@ $token = htmlspecialchars($token);
 
 $ipmode = isset($_GET["ipmode"]) ? $_GET["ipmode"] : ''; 
 $ipmode = htmlspecialchars($ipmode);
-switch ($ipmode) {
-    case "ipv4only":
-        $ipmode = "ipv4only";
-        break;
-    case "ipv6only":
-        $ipmode = "ipv6only";
-        break;
- 	default:
-        $ipmode = "all";
-        break;
-}
 
 $ipv6 = isset($_GET["ipv6"]) ? $_GET["ipv6"] : ''; 
 if (!filter_var($ipv6, FILTER_VALIDATE_IP, FILTER_FLAG_IPV6)) {
@@ -55,6 +44,20 @@ if (!filter_var($ipv4, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4)) {
 	$ipv4 = str_replace("::ffff:","",$_SERVER["REMOTE_ADDR"]);
 }
 
+switch ($ipmode) {
+    case "ipv4only":
+        $ipmode = "ipv4only";
+		$ipv6 = "::1";
+        break;
+    case "ipv6only":
+        $ipmode = "ipv6only";
+		$ipv4 = "127.0.0.1";
+        break;
+ 	default:
+        $ipmode = "all";
+        break;
+}
+
 if(!empty($token) && $token == $dynhost[$hostname]["token"]){
 	exec('sh/wrapper "1" "configs"',$out);
 	$outNew = array();
@@ -63,21 +66,38 @@ if(!empty($token) && $token == $dynhost[$hostname]["token"]){
 		if(!empty($tmp))$outNew[]=$tmp;
 	}
 
-	DEFINE("DB_PWD", $outNew[0]);
-	DEFINE("PSA_PATH", $outNew[1]);
-	DEFINE("VHOSTS_PATH", $outNew[2]);
+	DEFINE("DB_PWD",$outNew[0]);
+	DEFINE("PSA_PATH",$outNew[1]);
+	DEFINE("VHOSTS_PATH",$outNew[2]);
 	require("../config.inc.php");
 	require("../paa.class.php");
 	require("../dcp.class.php");
 
 	$dom = explode(".",$hostname);
 	$domain = $dom[count($dom)-2].".".$dom[count($dom)-1];
-
+	
 	$dcp = new dcp(new s(),$_GET,array(DB_HOST,DB_NAME,DB_USR,DB_PWD));	
 	$_POST["hostname"] = $hostname;
 	$dcp->deactivate();
 	$dcp->activate($ipv4,$dynhost[$hostname]["token"],$dynhost[$hostname]["orgipv4"],$ipv6,$dynhost[$hostname]["orgipv6"]);
 	$dcp->setSqlEntry($hostname,$ipv4,$ipv6,$ipmode);
+
+
+	// update also all subdomains 
+	foreach ($dynhost as $k => $v) {
+		if((strpos(strrev($k),strrev($hostname)) === 0) AND ($k != $hostname) AND ($dynhost[$k]["token"] == $token)){
+			// make new ipv6 with dynamic prefix part and fix interface identifier from setting ipv6iid
+			$ipv6iidmask = $dynhost[$k]["ipv6iid"];
+			$ipv6prefixmask = ($dynhost[$k]["ipv6prefixmask"] / 8);
+			$prefix_bin = substr(inet_pton($ipv6), 0 , $ipv6prefixmask);  
+			$suffix_bin = substr(inet_pton($ipv6iidmask), $ipv6prefixmask, (16 - $ipv6prefixmask));  
+			$ipv6iid = inet_ntop($prefix_bin.$suffix_bin);
+
+			$dcp->activate($ipv4,$dynhost[$k]["token"],$dynhost[$k]["orgipv4"],$ipv6iid,$dynhost[$k]["orgipv6"]);
+			$dcp->setSqlEntry($k,$ipv4,$ipv6iid,$ipmode);
+		}
+	}
+
 	exec('sh/wrapper "1" "refreshdns" "'.PSA_PATH.'/bin/dns" "'.$domain.'"');
 
 	echo "OK";
